28c317d89bd1da737154fbb481b147958ff4403f
abocquet <adrienbocquet38@gmail.com>
2017-11-15 16:52:24 +0100

    fixed display

--- display.py
+++ display.py
@@ -1,4 +1,4 @@
-def display(stdscr, field, caracters):
+def display(stdscr, field, characters):
     max_x = len(field[0])
     max_y = len(field)
 
@@ -6,22 +6,7 @@ def display(stdscr, field, caracters):
 
     for y in range(max_y):
         for x in range(max_x):
-            if field[y][x] == 1:
-                if y - 1 > 0 and y + 1 < max_y:
-                    if field[y - 1][x] == 0:
-                        stdscr.addstr(y - 1, x, chars[5])
-                    if field[y + 1][x] == 0:
-                        stdscr.addstr(y + 1, x, chars[5])
-                if x - 1 > 0 and x + 1 <max_x:
-                    if field[y][x - 1] == 0:
-                        stdscr.addstr(y, x - 1, chars[6])
-                    if field[y][x + 1] == 0:
-                        stdscr.addstr(y, x + 1, chars[6])
-                else:
-                    stdscr.addstr(y, x, chars[field[y][x]])
-            elif field[y][x] != 0:
-                stdscr.addstr(y, x, chars[field[y][x]])
+            stdscr.addstr(y, x, chars[field[y][x]])
 
-	for character in characters:
+    for character in characters:
         stdscr.addstr(character.get_y(), character.get_x(), character.get_display())
-

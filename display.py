60e3be0ac014e830a1f9128102ad074ac14ca456
martinho23 <ajoni844@gmail.com>
2017-11-15 16:44:37 +0100

    New display

--- display.py
+++ display.py
@@ -1,5 +1,4 @@
-
-def display(stdscr, field, characters):
+def display(stdscr, field, caracters):
     max_x = len(field[0])
     max_y = len(field)
 
@@ -8,16 +7,18 @@ def display(stdscr, field, characters):
     for y in range(max_y):
         for x in range(max_x):
             if field[y][x] == 1:
-                if y - 1 > 0 and y + 1 < max_y and (field[y - 1][x] == 0 or field[y + 1][x] == 0):
-                    stdscr.addstr(y + 1, x + 1, chars[5])
-                elif x - 1 > 0 and x + 1 < max_x and (field[y][x - 1] == 0 or field[y][x + 1] == 0):
-                    stdscr.addstr(y + 1, x + 1, chars[6])
+                if y - 1 > 0 and y + 1 < max_y:
+                    if field[y - 1][x] == 0:
+                        stdscr.addstr(y - 1, x, chars[5])
+                    if field[y + 1][x] == 0:
+                        stdscr.addstr(y + 1, x, chars[5])
+                if x - 1 > 0 and x + 1 <max_x:
+                    if field[y][x - 1] == 0:
+                        stdscr.addstr(y, x - 1, chars[6])
+                    if field[y][x + 1] == 0:
+                        stdscr.addstr(y, x + 1, chars[6])
                 else:
-                    stdscr.addstr(y + 1, x + 1, chars[field[y][x]])
-            else:
-                stdscr.addstr(y + 1, x + 1, chars[field[y][x]])
-
-
-    for character in characters:
-        stdscr.addstr(character.get_y(), character.get_x(), character.get_display())
+                    stdscr.addstr(y, x, chars[field[y][x]])
+            elif field[y][x] != 0:
+                stdscr.addstr(y, x, chars[field[y][x]])
 

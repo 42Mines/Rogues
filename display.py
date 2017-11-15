f8e1e60e966e824e7b8630a4943d5c25a87e70c8
abocquet <adrienbocquet38@gmail.com>
2017-11-15 14:55:36 +0100

    added display persistance

--- display.py
+++ display.py
@@ -10,43 +10,53 @@
 #                                                                              #
 # **************************************************************************** #
 
-import curses 
-#Initialisation of curses
+import curses
+
+# Initialisation of curses
 stdscr = curses.initscr()
 curses.noecho()
 curses.cbreak()
 stdscr.keypad(True)
 
-field = [   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,1,1,4,1,0,0,0,0,0,0,0,0,1,1,1,1,0,0],
-            [0,0,3,1,1,1,0,2,2,2,2,2,2,2,3,1,1,1,0,0],
-            [0,0,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+field = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 1, 1, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+         [0, 0, 3, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 3, 1, 1, 1, 0, 0],
+         [0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
 
 def display(stdscr, field, caracters):
-    x = len(field[0])
-    y = len(field)
-    i = 0;
-    j = 0;
-    chars = [" ",".","*","#","x","_","|"]
+    max_x = len(field[0])
+    max_y = len(field)
 
+    chars = [" ", ".", "*", "#", "x", "_", "|"]
 
-    for i in range(y):
-        for j in range(x):
-            if field[i][j] == 1:
-                if i - 1 > 0 and i + 1 < y and (field[i - 1][j] != 1 or field[i + 1][j] != 1):
-                    stdscr.addstr(i, j, chars[5])
-                elif i - 1 > 0 and i + 1 < x and (field[i][j - 1] != 1 or field[i][j + 1] != 1):
+    for y in range(max_y):
+        for x in range(max_x):
+            if field[y][x] == 1:
+                if y - 1 > 0 and y + 1 < y and (field[y - 1][x] != 1 or field[y + 1][x] != 1):
+                    stdscr.addstr(y, x, chars[5])
+                elif x - 1 > 0 and x + 1 < x and (field[y][x - 1] != 1 or field[y][x + 1] != 1):
                     stdscr.addstr(y, x, chars[6])
                 else:
-                    stdscr.add(i, j, chars[field[y][x]])
+                    stdscr.addstr(y, x, chars[field[y][x]])
             else:
-                 strscr.add(i, j, chars[field[y][x]])
+                stdscr.addstr(y, x, chars[field[y][x]])
 
-display(strscr, field, [])
-#QExitiong curses
+
+display(stdscr, field, [])
+
+
+while True:
+    key = stdscr.getch()
+    print(key)
+    if key == ord('a'):
+        break
+
+# QExitiong curses
 curses.nocbreak()
-stdscr.Keypad(False)
+stdscr.keypad(False)
 curses.echo()
 curses.endwin()
+
